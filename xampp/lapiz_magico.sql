890a33ecb815c01f4c3659f26a714ffb7c9f0997
Lucas Cossari <cossari.lu@gmail.com>
2025-09-19 21:18:16 -0300

    fusioné eventos y participantes de mati y agregé miembro? en usuario

--- xampp/lapiz_magico.sql
+++ xampp/lapiz_magico.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-09-2025 a las 21:29:54
+-- Tiempo de generación: 20-09-2025 a las 02:15:23
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -21,13 +21,19 @@ SET time_zone = "+00:00";
 -- Base de datos: `lapiz_magico`
 --
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autor`
+--
+
 CREATE TABLE `autor` (
   `id_autor` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `apellido` varchar(100) NOT NULL,
   `nacionalidad` varchar(50) DEFAULT NULL,
   `fecha_nacimiento` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `autor`
@@ -41,6 +47,20 @@ INSERT INTO `autor` (`id_autor`, `nombre`, `apellido`, `nacionalidad`, `fecha_na
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `evento`
+--
+
+CREATE TABLE `evento` (
+  `id_evento` int(11) NOT NULL,
+  `nombre` varchar(150) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `fecha` date NOT NULL,
+  `id_libro` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `genero`
 --
 
@@ -48,7 +68,7 @@ CREATE TABLE `genero` (
   `id_genero` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `descripcion` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `genero`
@@ -72,16 +92,55 @@ CREATE TABLE `libros` (
   `anio_publicacion` year(4) DEFAULT NULL,
   `id_genero` int(11) DEFAULT NULL,
   `id_autor` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `libros`
 --
 
 INSERT INTO `libros` (`id_libro`, `titulo`, `isbn`, `anio_publicacion`, `id_genero`, `id_autor`) VALUES
-(1, 'Cien Años de Soledad', '978-0307474728', 1967, 1, 1),
-(2, 'Fundación', '978-0553293357', 1951, 2, 2),
-(3, 'It', '978-1501142970', 1986, 1, 3);
+(1, 'Cien Años de Soledad', '978-0307474728', '1967', 1, 1),
+(2, 'Fundación', '978-0553293357', '1951', 2, 2),
+(3, 'It', '978-1501142970', '1986', 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `participantes_evento`
+--
+
+CREATE TABLE `participantes_evento` (
+  `id_participacion` int(11) NOT NULL,
+  `id_evento` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `rol` enum('invitado','organizador') DEFAULT 'invitado'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `apellido_2` varchar(50) DEFAULT NULL,
+  `telefono` varchar(50) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `miembro?` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `apellido_2`, `telefono`, `email`, `password`, `miembro?`) VALUES
+(1, 'lucas', 'cossari', NULL, '1132591231', 'lucascossari@gmail.com', '1234', 0),
+(5, 'mati', 'figue', NULL, '1234', 'mati@gmail.com', '1234', 0),
+(6, 'ludmi', 'cristaldo', NULL, '111111111111111', 'ludmi@gmail.com', '1234', 0);
 
 --
 -- Índices para tablas volcadas
@@ -92,6 +151,13 @@ INSERT INTO `libros` (`id_libro`, `titulo`, `isbn`, `anio_publicacion`, `id_gene
 --
 ALTER TABLE `autor`
   ADD PRIMARY KEY (`id_autor`);
+
+--
+-- Indices de la tabla `evento`
+--
+ALTER TABLE `evento`
+  ADD PRIMARY KEY (`id_evento`),
+  ADD KEY `id_libro` (`id_libro`);
 
 --
 -- Indices de la tabla `genero`
@@ -109,6 +175,20 @@ ALTER TABLE `libros`
   ADD KEY `id_autor` (`id_autor`);
 
 --
+-- Indices de la tabla `participantes_evento`
+--
+ALTER TABLE `participantes_evento`
+  ADD PRIMARY KEY (`id_participacion`),
+  ADD KEY `id_evento` (`id_evento`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -117,6 +197,12 @@ ALTER TABLE `libros`
 --
 ALTER TABLE `autor`
   MODIFY `id_autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `evento`
+--
+ALTER TABLE `evento`
+  MODIFY `id_evento` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `genero`
@@ -131,8 +217,26 @@ ALTER TABLE `libros`
   MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT de la tabla `participantes_evento`
+--
+ALTER TABLE `participantes_evento`
+  MODIFY `id_participacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `evento`
+--
+ALTER TABLE `evento`
+  ADD CONSTRAINT `evento_ibfk_1` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id_libro`);
 
 --
 -- Filtros para la tabla `libros`
@@ -140,56 +244,13 @@ ALTER TABLE `libros`
 ALTER TABLE `libros`
   ADD CONSTRAINT `libros_ibfk_1` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`id_genero`),
   ADD CONSTRAINT `libros_ibfk_2` FOREIGN KEY (`id_autor`) REFERENCES `autor` (`id_autor`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
--- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Filtros para la tabla `participantes_evento`
 --
-
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `apellido` varchar(50) NOT NULL,
-  `apellido_2` varchar(50) DEFAULT NULL,
-  `telefono` varchar(50) DEFAULT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `apellido_2`, `telefono`, `email`, `password`) VALUES
-(1, 'lucas', 'cossari', NULL, '1132591231', 'lucascossari@gmail.com', '1234'),
-(5, 'mati', 'figue', NULL, '1234', 'mati@gmail.com', '1234'),
-(6, 'ludmi', 'cristaldo', NULL, '111111111111111', 'ludmi@gmail.com', '1234');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `participantes_evento`
+  ADD CONSTRAINT `participantes_evento_ibfk_1` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`id_evento`),
+  ADD CONSTRAINT `participantes_evento_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
